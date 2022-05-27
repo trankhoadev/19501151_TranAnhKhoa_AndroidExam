15235fdf8b999b271b39e19c8a89c3d9fa5d434b
trankhoadev <trankhoadev@gmail.com>
2022-05-27 08:29:01 +0700

    Hoàn thành được giao diện đăng nhập và danh sách khóa học, bắt đầu xử lí

---
+++ 19501151_TranAnhKhoa_AndroidExam/app/src/main/java/com/example/a19501151_trananhkhoa_androidexam/ListBussinessScreen.java
@@ -0,0 +1,14 @@
+package com.example.a19501151_trananhkhoa_androidexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListBussinessScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_bussiness_screen);
+    }
+}
