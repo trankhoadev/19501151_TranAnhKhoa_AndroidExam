e89b8f66ca325ba05d40531ba1104be9ba7d5a6c
trankhoadev <trankhoadev@gmail.com>
2022-05-27 07:36:38 +0700

    Tạo mới một project rỗng, bắt đầu thực hiện làm bài

---
+++ 19501151_TranAnhKhoa_AndroidExam/app/src/main/java/com/example/a19501151_trananhkhoa_androidexam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a19501151_trananhkhoa_androidexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
