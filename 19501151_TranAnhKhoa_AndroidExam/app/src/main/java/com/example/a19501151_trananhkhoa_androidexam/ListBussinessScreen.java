29ec6a8131947dd8effcc50a3847c178722718c0
trankhoadev <trankhoadev@gmail.com>
2022-05-27 09:12:30 +0700

    Cập nhật lại lần cuối các file thiết kế và minh chứng

--- 19501151_TranAnhKhoa_AndroidExam/app/src/main/java/com/example/a19501151_trananhkhoa_androidexam/ListBussinessScreen.java
+++ 19501151_TranAnhKhoa_AndroidExam/app/src/main/java/com/example/a19501151_trananhkhoa_androidexam/ListBussinessScreen.java
@@ -2,13 +2,24 @@ package com.example.a19501151_trananhkhoa_androidexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListBussinessScreen extends AppCompatActivity {
+
+    private View btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_bussiness_screen);
+
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(v -> {
+            startActivity(new Intent(ListBussinessScreen.this, Bussiness.class));
+        });
     }
 }
